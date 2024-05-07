9d54aceadd8b85471803193523b30e2f11280dea
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-07 16:09:13 -0300

    Create ex002.py

---
+++ exercícios/ex002.py
@@ -0,0 +1,4 @@
+dia = input('Qual é o dia que você nasceu? ')
+mes = input('Qual é o mês que você nasceu? ')
+ano = input('Qual é o ano que você nasceu? ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano + '.', 'Correto?')
