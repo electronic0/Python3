d9f7fa8cefa7e0b97f850bcb42184f7730689c7e
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-08 13:34:36 -0300

    Create ex004.py

---
+++ exercícios/mundo-1/ex004.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite segundo valor: '))
+print(f'A soma dos valores é {n1 + n2} \nA subtração dos valores é {n1 - n2} \nA divisão dos valores é {n1 / n2 :.3f} \nA multiplicação dos valores é {n1 * n2}')
