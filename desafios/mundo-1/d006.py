ab42ee1db405bc020adb61678458d9f549a90c34
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-08 15:52:54 -0300

    Update d006.py

--- desafios/mundo-1/d006.py
+++ desafios/mundo-1/d006.py
@@ -1,2 +1,2 @@
 num = int(input('Digite um numero: '))
-print(f'O dobro desse numero é {num * 2}, o triplo desse numero é {num * 3} e a raiz quadrada desse numero é {num ** (1 / 2)}')
+print(f'O dobro desse numero é {num * 2}, o triplo desse numero é {num * 3} e a raiz quadrada desse numero é {num ** (1 / 2):.2f}')
