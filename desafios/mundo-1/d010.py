cbfe222f8d45c282678f807337f52260d31c66d8
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-08 17:26:22 -0300

    Update d010.py

--- desafios/mundo-1/d010.py
+++ desafios/mundo-1/d010.py
@@ -1,2 +1,2 @@
 valor = float(input('Quantos dinheiro você possui nesse momento? R$'))
-print(f'Com esse dinheiro você pode comprar ${valor / 3.27:.2f} dólares.')
+print(f'Com esse dinheiro você pode comprar ${valor / 5.09:.2f} dólares ou comprar £{valor / 5.47:.2f}.')
