5fa57b655a3f809b1628bd6a9bfa42b6d16c4a58
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-08 16:27:01 -0300

    Update d008.py

--- desafios/mundo-1/d008.py
+++ desafios/mundo-1/d008.py
@@ -1,2 +1,2 @@
 valor = int(input('Digite um valor em metros: '))
-print(f'O valor convertido em centímetros vale {valor * 100}cm \nE o valor convertido em milímetros vale {valor * 1000}mm')
+print(f'O valor convertido em centímetros vale {valor * 100:.0f}cm \nE o valor convertido em milímetros vale {valor * 1000:.0f}mm')
