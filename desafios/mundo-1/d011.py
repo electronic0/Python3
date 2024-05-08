3520aaab7404796b4af075078e16cc9974e756c9
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-08 14:30:44 -0300

    Create d011.py

---
+++ desafios/mundo-1/d011.py
@@ -0,0 +1,4 @@
+l = float(input('Qual é a largura da sua parede em metros? '))
+a = float(input('Qual é altura da sua parede em metros? '))
+area = l * a
+print(f'Com base dos valores dado, sua area é {area}m² \nPara pintar as paredes, é necessario usar {area / 2} litros de tinta.')
