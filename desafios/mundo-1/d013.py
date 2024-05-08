fc299b7db3e544102c033ca52aaa4c66330806cf
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-08 19:25:54 -0300

    Update d013.py

--- desafios/mundo-1/d013.py
+++ desafios/mundo-1/d013.py
@@ -1,3 +1,3 @@
 s = float(input('Qual é o salário do Funcionário? R$'))
-a = s * 115 / 100
+a = s + (s * 15 / 100)
 print(f'O salário do funcionário, com um aumento de 15%, passa receber R${a:.2f}')
