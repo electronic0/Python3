20eb029d11a9b2d9eca626cf7e0c4c5093d7085f
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-08 10:14:02 -0300

    Update ex003.py

--- exercícios/mundo-1/ex003.py
+++ exercícios/mundo-1/ex003.py
@@ -1,4 +1,4 @@
-n1 = int(input('Digite um numero '))
-n2 = int(input('Digite outro numero '))
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro numero: '))
 soma = n1 + n2
-print('A soma de total dos numeros deu', soma)
+print(f'A soma de {n1} e {n2} deu {soma}')
