ac3aad6963e6640a3b80f12920f9f50c3e8cf0f0
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-08 15:59:44 -0300

    Update d007.py

--- desafios/mundo-1/d007.py
+++ desafios/mundo-1/d007.py
@@ -1,3 +1,3 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-print(f'A media dos dois valores digitados é {(n1 + n2) / 2}')
+n1 = float(input('Digite um valor: '))
+n2 = float(input('Digite outro valor: '))
+print(f'A media dos dois valores digitados é {(n1 + n2) / 2:.1f}')
