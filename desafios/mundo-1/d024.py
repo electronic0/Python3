1ad992fb85dc54d4694f79718650d32ed47af005
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-10 12:07:27 -0300

    Update d024.py

--- desafios/mundo-1/d024.py
+++ desafios/mundo-1/d024.py
@@ -1,3 +1,2 @@
 cidade = str(input('Digite um nome de uma cidade: '))
-santo = cidade.split()[0].upper()
-print(f'O nome da cidade começa com a palavra Santo? {'SANTO' in santo}')
+print(f'O nome da cidade começa com a palavra Santo? {cidade.split()[0].upper() == 'SANTO'}')
