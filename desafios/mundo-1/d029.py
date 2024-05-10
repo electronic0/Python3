4c36802d9670d99861cfc83fc4ff4f7e907a9744
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-10 19:35:52 -0300

    Create d029.py

---
+++ desafios/mundo-1/d029.py
@@ -0,0 +1,6 @@
+vel = int(input('Qual é a velocidade do carro no momento? '))
+multa = 7 * (vel - 80)
+if vel >= 80:
+    print(f'Você foi multado por ultrapassar na velocidade de 80Km/h. A multa vai custar R${multa}')
+else:
+    print('Obrigado por respeitar as leis de trânsito! Você esta na velocidade abaixo de 80Km/h')
