0030eb00ab9358b76910062765cf63501da74647
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-11 16:19:13 -0300

    Update d031.py

--- desafios/mundo-1/d031.py
+++ desafios/mundo-1/d031.py
@@ -1,6 +1,6 @@
 print('Uma companhia aérea vai fazer uma viagem de avião combrando passagens de R$0,50 por Km/h e R$0,45 para viagens acima de 200Km.')
 d = int(input('Digite uma distância de uma viagem de avião em Km: '))
 if d > 200:
-    print(f'A passagem da viagem vai custar R${d * 0.45:.0f}')
+    print(f'A passagem da viagem vai custar R${d * 0.45:.2f}')
 else:
-    print(f'A passagem da viagem vai custar R${d * 0.5:.0f}')
+    print(f'A passagem da viagem vai custar R${d * 0.5:.2f}')
