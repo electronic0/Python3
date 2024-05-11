5af226e692ce57f229112f6e5e7e3ceaa64c9ef3
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-11 16:30:52 -0300

    Update d032.py

--- desafios/mundo-1/d032.py
+++ desafios/mundo-1/d032.py
@@ -1,5 +1,8 @@
-ano = int(input('Qual ano nós estamos? '))
-if ano % 4 == 0:
+from datetime import date
+ano = int(input('Qual ano nós estamos? Coloque 0 para analisar o ano atual: '))
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('Estamos no ano bissexto!')
 else:
     print('Obrigado!')
