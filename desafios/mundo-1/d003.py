95488476e95e7985a48324a0700633fe35814c08
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-12 18:19:20 -0300

    Update d003.py

--- desafios/mundo-1/d003.py
+++ desafios/mundo-1/d003.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro valor: '))
 soma = n1 + n2
-print(f'A soma dos numeros {n1} e {n2} vale {soma}')
+print(f'A soma dos numeros {'\033[34m'}{n1} e {n2}{'\033[m'} vale {'\033[32m'}{soma}{'\033[m'}')
