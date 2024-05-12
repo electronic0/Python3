2dc16e8ef65a17e5ce317a7f94e4d052c9a47ab2
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-12 18:16:01 -0300

    Update d002.py

--- desafios/mundo-1/d002.py
+++ desafios/mundo-1/d002.py
@@ -1,2 +1,2 @@
 nome = input('Qual é o seu nome? ')
-print(f'Prazer de conhecer, {nome}! Seja bem-vindo ao mundo Python.')
+print(f'Prazer de conhecer, {'\033[34m'}{nome}!{'\033[m'} Seja bem-vindo ao mundo Python.')
