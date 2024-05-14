3197c49bf3ae5bf9edd87317a26f9cfee62d1d09
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-14 11:10:09 -0300

    Create d037.py

---
+++ desafios/mundo-2/d037.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um número: '))
+conv = int(input('Digite um número que corresponde sua base de conversão \n- 1 para binário\n- 2 para octal\n- 3 para hexadecimal\n: '))
+if conv == 1:
+    print(f'O número {num} convertido para binário fica {bin(num)}')
+elif conv == 2:
+    print(f'O número {num} convertido para octal fica {oct(num)}')
+else:
+    print(f'O número {num} convertido para hexadecimal fica {hex(num)}')
