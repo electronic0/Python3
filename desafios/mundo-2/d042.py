c24d8b95daf71aa3cf0cb731efbdf752b82ba300
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-15 14:20:21 -0300

    Update d042.py

--- desafios/mundo-2/d042.py
+++ desafios/mundo-2/d042.py
@@ -3,11 +3,11 @@ c2 = float(input('Digite o segundo valor do comprimento: '))
 c3 = float(input('Digite o terceiro valor do comprimento: '))
 if c1 < c2 + c3 and c2 < c1 + c3 and c3 < c1 + c2:
     print('Com base nos valores dado, é possivel formar um triangulo!')
-    if c1 == c2 and c1 == c3:
+    if c1 == c2 == c3:
         print('O triangulo vai ser Equilátero.')
-    elif c1 == c2 or c2 == c3:
-        print('O triangulo vai ser Isósceles.')
+    elif c1 != c2 != c3 != c1:
+        print('O triangulo vai ser Escaleno.')
     else:
-        print('O trinagulo vai ser Escaleno.')
+        print('O trinagulo vai ser Isósceles.')
 else:
     print('Com base nos valores dado, não é possivel formar um triangulo, pois um lado do comprimento esta maior ou menor do que os outros.')
