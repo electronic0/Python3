7768bcdb9639c6ed77156644b5b28f6d79635ac3
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-15 10:27:50 -0300

    Update d041.py

--- desafios/mundo-2/d041.py
+++ desafios/mundo-2/d041.py
@@ -3,11 +3,11 @@ ano = int(input('Qual é seu ano de nascimento? '))
 idade = date.today().year - ano
 if idade <= 9:
     print(f'O atleta de {idade} anos está na categoria Mirim')
-elif idade > 9 and idade <= 14:
+elif idade <= 14:
     print(f'O atleta de {idade} anos está na categoria Infantil')
-elif idade > 14 and idade <= 19:
+elif idade <= 19:
     print(f'O atleta de {idade} anos está na categoria Junior')
-elif idade > 19 and idade <= 20:
+elif idade <= 25:
     print(f'O atleta de {idade} anos está na categoria Sênior')
 else:
     print(f'O atleta de {idade} anos está na categoria Master')
