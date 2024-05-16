0cef08b3c8138970233936b63f572eac4e521545
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-16 09:40:30 -0300

    Update ex008.py

--- exercícios/mundo-2/ex008.py
+++ exercícios/mundo-2/ex008.py
@@ -1,6 +1,6 @@
 nome = str(input('Qual é o seu nome? '))
-if nome == 'Pedro':
+if nome == 'Ronaldo':
     print(f'Que nome bonito!')
-elif nome in 'Juliano Renata Stephano':
+elif nome in 'Pedro Paulo Gabriel':
     print('Seu nome é bem popular')
 print(f'Tenha um bom dia, {nome}')
