38a50b47283b97fad1245dc1e2e316b005877700
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-17 19:10:35 -0300

    Create d062.py

---
+++ desafios/mundo-2/d062.py
@@ -0,0 +1,11 @@
+pt = int(input('Digite o valor do primeiro termo: '))
+r = int(input('Digite o valor da razão: '))
+d = pt + (10 - 1) * r
+while d > 0:
+    print(d, end = ' ')
+    d -= r
+    if d == 0:
+        c = int(input('\nQuantos termos que você quer ver mais? '))
+        d = pt + (c + 9) * r
+        if c == 0:
+            d = 0
