c6e1d77a7a5a7de05ef384c04a86c1213daed4e6
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-17 19:43:01 -0300

    Create d064.py

---
+++ desafios/mundo-2/d064.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um numero: '))
+c, s = 0, 0
+while n != 999:
+    n = int(input('Digite um valor qualquer (se digitar o valor 999 o programa para): '))
+    if n != 999:
+        c += 1
+        s = n + n
+print(f'Foram digitados {c} valores que, ao somar os números, dá o resultado de {s}')
