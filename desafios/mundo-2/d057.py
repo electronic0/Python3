ba61888f95217412099a7f49b54c97e0325a1dba
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-21 11:01:44 -0300

    Update d057.py

--- desafios/mundo-2/d057.py
+++ desafios/mundo-2/d057.py
@@ -2,3 +2,4 @@ sexo = str(input('Qual é o seu genero? [M/F] ')).upper().strip()
 while sexo != 'M' and sexo != 'F':
     print('Sexo invalido. Digite novamente')
     sexo = str(input('Qual é o seu genero? [M/F] ')).upper().strip()
+print('Registrado com sucesso!')
