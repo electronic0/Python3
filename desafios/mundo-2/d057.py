05a9750868e900c660a384c2936195eaf57f9db9
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-21 10:59:57 -0300

    Update d057.py

--- desafios/mundo-2/d057.py
+++ desafios/mundo-2/d057.py
@@ -1,4 +1,4 @@
-sexo = str(input('Qual é o seu genero? [M/F] ')).upper()
+sexo = str(input('Qual é o seu genero? [M/F] ')).upper().strip()
 while sexo != 'M' and sexo != 'F':
     print('Sexo invalido. Digite novamente')
-    sexo = str(input('Qual é o seu genero? [M/F] ')).upper()
+    sexo = str(input('Qual é o seu genero? [M/F] ')).upper().strip()
