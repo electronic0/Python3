2595fbc7ef3156fac09c05e37f3b61e5258cfa65
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-22 12:19:31 -0300

    Create d065.py

---
+++ desafios/mundo-2/d065.py
@@ -0,0 +1,16 @@
+s = 'S'
+q = soma = m = maior = menor = 0
+while s != 'N':
+    n = int(input('Digite um número: '))
+    q += 1
+    soma += n
+    if q == 1:
+        maior = menor = n
+    else:
+        if n > maior:
+            maior = n
+        elif n < menor:
+            menor = n
+    s = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    m = soma / q
+print(f'Voce digitou {q} valores e a media entre eles é {m} \nO maior número digitado foi o {maior} e o menor número digitado foi {menor}')
