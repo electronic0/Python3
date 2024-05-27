80a40e0e2404d479d4843f9c35a08566e5821126
electronic <78802199+electronic0@users.noreply.github.com>
2024-05-27 13:30:15 -0300

    Create d074.py

---
+++ desafios/mundo-3/d074.py
@@ -0,0 +1,3 @@
+from random import randint
+n = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print(f'Os valores sorteados foram: {n} \nO maior número foi {max(n)} e o menor número foi {min(n)}')
