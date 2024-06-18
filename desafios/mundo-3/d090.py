40086577e267f225b8ffbb65b531b3b3705f6446
electronic <78802199+electronic0@users.noreply.github.com>
2024-06-18 16:19:58 -0300

    Update d090.py

--- desafios/mundo-3/d090.py
+++ desafios/mundo-3/d090.py
@@ -1,13 +1,9 @@
-Dados = {}
-Nome = str(input('Insira o nome: ')).strip()
-Media = float(input(f'Insira a media do {Nome}: '))
-Situação = ''
-if Media > 6:
-    Situação = 'Aprovado'
+dados = {}
+dados['Nome'] = str(input('Insira o nome: ')).strip()
+dados['Media'] = float(input(f'Insira a media do {dados['Nome']}: '))
+if dados['Media'] >= 7:
+    dados['Situação'] = 'Aprovado'
 else:
-    Situação = 'Reaprovado'
-Dados['Nome'] = Nome
-Dados['Media'] = Media
-Dados['Situação'] = Situação
-for k, v in Dados.items():
+    dados['Situação'] = 'Reaprovado'
+for k, v in dados.items():
     print(f'{k} do aluno é {v}')
