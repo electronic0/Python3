dc4424259ea719e0a1282c8cfc915e62c12191ab
electronic <78802199+electronic0@users.noreply.github.com>
2024-06-19 13:16:08 -0300

    Update d092.py

--- desafios/mundo-3/d092.py
+++ desafios/mundo-3/d092.py
@@ -1,14 +1,11 @@
 from datetime import date
 dados = {}
-while True:
-    dados['Nome'] = str(input('Digite o nome: ')).strip()
-    dados['Idade'] = date.today().year - int(input('Ano de nascimento: '))
-    dados['CTPS'] = int(input('Carteira de Trabalho (digite 0 caso não tenha): '))
-    if dados['CTPS'] == 0:
-        break
+dados['Nome'] = str(input('Digite o nome: ')).strip()
+dados['Idade'] = date.today().year - int(input('Ano de nascimento: '))
+dados['CTPS'] = int(input('Carteira de Trabalho (digite 0 caso não tenha): '))
+if dados['CTPS'] != 0:
     dados['Contratação'] = int(input('Ano de contratação: '))
     dados['Salario'] = float(input('Salario R$'))
     dados['Aposentatoria'] = dados['Idade'] - (date.today().year - dados['Contratação']) + 35
-    break
 for k, v in dados.items():
     print(f'{k}: {v}')
