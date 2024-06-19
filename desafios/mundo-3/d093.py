ce11ac43cd72552efdd18628454c31d3870cc51b
electronic <78802199+electronic0@users.noreply.github.com>
2024-06-19 13:44:59 -0300

    Update d093.py

--- desafios/mundo-3/d093.py
+++ desafios/mundo-3/d093.py
@@ -1,16 +1,13 @@
-dados, lista, qg = {}, [], 0
-nome = str(input('Nome do jogador: '))
-dados['Nome'] = nome
-partidas = int(input(f'Quantas partidas o {nome} jogou? '))
+dados, lista = {}, []
+dados['Nome'] = str(input('Nome do jogador: '))
+partidas = int(input(f'Quantas partidas o {dados['Nome']} jogou? '))
 for p in range(1, partidas + 1):
-    g = int(input(f'Quantos gols na {p}° partida? '))
-    lista.append(g)
-    qg += g
+    lista.append(int(input(f'Quantos gols na {p}° partida? ')))
 dados['Gols'] = lista
-dados['Total'] = qg
-for k, v in dados.items():
-    print(f'{k}: {v}')
-print(f'O jogador {nome} jogou {partidas} partidas.')
+dados['Total'] = sum(lista)
+for c, v in dados.items():
+    print(f'{c}: {v}')
+print(f'O jogador {dados['Nome']} jogou {partidas} partidas.')
 for p in range(1, partidas + 1):
     print(f'Na {p}° partida, fez {lista[p - 1]} gols')
 print(f'Foi um total de {dados['Total']} gols')
