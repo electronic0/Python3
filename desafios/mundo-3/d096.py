bac9927023b9be4b1570d4d661a0e062c80a492e
electronic <78802199+electronic0@users.noreply.github.com>
2024-06-24 11:01:02 -0300

    Create d096.py

---
+++ desafios/mundo-3/d096.py
@@ -0,0 +1,6 @@
+def area():
+    lar = float(input('Largura (m): '))
+    com = float(input('Comprimento (m): '))
+    a = lar * com
+    print(f'A área de um terreno {lar} x {com} vale {a}m²')
+area()
